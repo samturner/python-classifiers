1c102caf6549def17ddf4a958d1a8de22f53be2e
Sam Turner <samturner695@gmail.com>
2014-04-18 16:09:54 +1000

    Normalised the CSV

--- 2. processing/preprocessor.py
+++ 2. processing/preprocessor.py
@@ -1,18 +1,13 @@
 # add the header line
-f = open("pima.csv", "w")
+f = open("pima_processed.csv", "w")
 
-i = 0;
+f.write('num_pregnant,glucose_concentration,blood_pressure,tricep_skin,serum_insulin,BMI,diabetes_pedigree,age,class\n')	# write out the header file
 
-f.write('num_pregnant,glucose_concentration,blood_pressure,tricep_skin,serum_insulin,BMI,diabetes_pedigree,age,class\n')
 for line in open("pima-indians-diabetes.data", "rU"):
-	lineArray = line.strip().split(",")
-	lineArray[-1] = "class" + lineArray[-1]; # add class to last element
-	
+	lineArray = line.strip().split(",")		# split into an array
+	lineArray[-1] = "class" + lineArray[-1]; 	# add class to last element
 
-	if int(lineArray[2]) and int(lineArray[1]) and int(float(lineArray[5])) and int(float(lineArray[3])):
+	if int(lineArray[2]) and int(lineArray[1]) and int(float(lineArray[5])) and int(float(lineArray[3])):	# remove missing values from appropriate attributes
 		f.write(",".join(lineArray) + "\n")
-		i += 1;
 		
 f.close();
-
-print i
